b7a5b244aa394a2b22490ac6a798eaacb68ce4ca
Caio Peluti <cpeluti@gmail.com>
2022-08-09 20:57:04 -0300

    Mongoose adicionado

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,9 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+const dbUser = process.env.DB_USER;
+const dbPasswd = process.env.DB_PASSWORD;
+mongoose_1.default.connect(`mongodb+srv://${dbUser}:${dbPasswd}@cluster0.ub24ryp.mongodb.net/?retryWrites=true&w=majority`);
+const Cat = mongoose_1.default.model('Cat', { name: String });
+const kitty = new Cat({ name: 'teste' });
+kitty.save().then(() => console.log('foi'));
 app.get('/', (req, res) => {
     res.send('Servidor express com typescript');
 });
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,15 @@
 import express, {Express, Request, Response} from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
+const dbUser = process.env.DB_USER
+const dbPasswd = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPasswd}@cluster0.ub24ryp.mongodb.net/?retryWrites=true&w=majority`)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Servidor express com typescript')
