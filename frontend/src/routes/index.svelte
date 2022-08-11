4ab4e838c05707940b47c2bcac38ddbb0440f484
Isaque <isak369@hotmail.com>
2022-08-11 16:03:43 -0300

    Termos mudados de portugues para ingles

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     import type Colecionaveis from "src/interfaces/Colecoes";
-    let colecionavel: Colecionaveis = {nome: "d", raridade: "d", info: "d"}
-    let colecionaveis: Array<Colecionaveis>
-        
+    let colecionavel: Colecionaveis = {name: "d", rarity: "d", about: "d"}
+
 
 
 </script>
-<h1>Welcome {colecionavel.nome}</h1>
+<h1>Welcome {colecionavel.name}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
