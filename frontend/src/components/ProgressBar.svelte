7e1a0a11682ee279f74f9408a1f92398dc72fe28
IsaqueA <84244850+seraphritt@users.noreply.github.com>
2022-08-29 16:59:52 -0300

    adicionando progress bar

--- frontend/src/components/ProgressBar.svelte
+++ frontend/src/components/ProgressBar.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-    export let progress    
+    export let progress;    
 </script>
-<component/>
 <div style="background-color:lightgray">
     <div style="background-color:greenyellow; height: 24px; width: {progress}%">
-    </div>
+</div>
 </div>
 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import HuntCard from "../components/HuntCard.svelte"
     import type { Hunt } from "src/interfaces/Hunt";
+    import ProgressBar from "../components/ProgressBar.svelte";
     const hunts:Array<Hunt> = 
         [{name: "string",
         location: "string",
@@ -27,8 +28,10 @@
         latlong: [13821839128, -21319038]}, {name: "string2",
         location: "string2",
         latlong: [1382183239128, -213190238]}]
+    const progress = 0.9
 </script>
 {#each hunts as hunt}
     <HuntCard hunt={hunt}/>
+    <ProgressBar progress={50}/>
 
 {/each}
