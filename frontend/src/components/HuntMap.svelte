bd57ee7f7a6656815dc19eadded55f27881ba6e4
Isaque <isak369@hotmail.com>
2022-09-06 14:40:25 -0300

    package-lock.json e package.json modificados

---
+++ frontend/src/components/HuntMap.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">export let hunt;</script>
+<main>
+
+    <div id="googleMap" style="width:100%;height:400px;overflow:visible;"></div>
+    <div id="information">{hunt.latlong[0]} {hunt.latlong[1]}</div>
+    <script lang="js">
+        var coords = document.getElementById("information").innerText.split(" ")
+        console.log(coords)
+        function myMap() {
+            var mapProp= {
+            center:new google.maps.LatLng(coords[0], coords[1]),
+            zoom:4,
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+        }
+
+    </script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDT1VWuFBpuLtdeThp9rV8g8Jg8OhW_Cz8&callback=myMap"></script>
+   <!--https://maps.googleapis.com/maps/api/js?v=3.exp&hl=en&key=AIzaSyDT1VWuFBpuLtdeThp9rV8g8Jg8OhW_Cz8&callback=myMap -->
+</main>
---
+++ frontend/src/routes/hunt/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import HuntMap from "../../components/HuntMap.svelte"
+    import type { Hunt } from "src/interfaces/Hunt";
+    const hunts:Array<Hunt> = 
+        [{name: "string",
+        location: "string",
+        latlong: [-15.884039920347867, -48.01590311958394]}]
+</script>
+{#each hunts as hunt}
+    <HuntMap hunt={hunt}/>
+{/each}
