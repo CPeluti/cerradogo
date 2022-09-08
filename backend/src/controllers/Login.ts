a0ae94a94b80eee1d42b4b0c88c0639101bc3645
Isaque <isak369@hotmail.com>
2022-09-07 21:58:32 -0300

    login funcional

--- backend/src/controllers/Login.ts
+++ backend/src/controllers/Login.ts
@@ -3,15 +3,18 @@ import authentication from '../helpers/authentication'
 import dotenv from 'dotenv'
 dotenv.config()
 const key = "teste"
+const age: number = process.env.EXPIRESIN ? parseInt(process.env.EXPIRESIN) : 15000
 const create = async (req: Request, res: Response) => {
   console.log(req.body)
   const {username, password} = req.body
     try{
+        console.log("a")
         const token = await authentication.auth({username, password}, key)
+        console.log("")
         res.cookie("session", token.token, {
           path: '/',
           httpOnly: true,
-          maxAge: process.env.EXPIRESIN,
+          maxAge: age,
         })
         res.send({user: token.user})
   } catch(e){
